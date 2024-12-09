917293ae1824bf14d0b48887b1ad5854eb9185f3
Marcos Gabriel <marcosindev@gmail.com>
2024-12-09 14:33:50 -0300

    Sass para CSS

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-import styles from './Navbar.module.scss'
+import styles from './Navbar.module.css'
 
 function Navbar() {
 
--- src/components/pages/animations/AnimationHome.js
+++ src/components/pages/animations/AnimationHome.js
@@ -4,7 +4,7 @@ import Iphone1 from '../../../img/Black iPhone 15 PNG.png';
 import Iphone2 from '../../../img/Blue iPhone 15 PNG.png';
 import Iphone3 from '../../../img/Green iPhone 15 PNG.png';
 import Iphone4 from '../../../img/Yellow iPhone 15 PNG.png';
-import styles from './Animation.module.scss';
+import styles from './Animation.module.css';
 
 const AnimationHome = () => {
 
