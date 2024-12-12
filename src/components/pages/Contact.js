79cbe0c55313211427c1f4934f7c9abd530b2ff4
Marcos Gabriel <marcosindev@gmail.com>
2024-12-12 15:21:09 -0300

    Projeto completo

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,24 @@
+import styles from './Contact.module.css'
+
 function Contact() {
 
+    function submitDoubt() {
+        alert('Dúvida enviada!')
+    }
+
     return (
-        <>Olá 3</>
+        <form className={styles.form_container}>
+            <div className={styles.form_content}>
+                <h2>Qual sua dúvida?</h2>
+                <input type="text" placeholder="Qual seu nome?">
+                </input>
+                <input type="email" placeholder="Digite seu e-mail.">
+                </input>
+                <input type="textarea" rows="5" cols="30" placeholder="Escreva sua dúvida.">
+                </input>
+                <button type="submit" className={styles.button_submit} onClick={submitDoubt}>Enviar</button>
+            </div>
+        </form>
     )
 }
 
